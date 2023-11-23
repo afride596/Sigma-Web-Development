7a9dd256bfcde30627669f6d2da653dbff3eabc2
afide596 <afride596@gmail.com>
2023-11-23 19:20:42 +0530

    learn all type of loops

---
+++ Javascript/loops/index.js
@@ -0,0 +1,46 @@
+
+// let array=[4,45,6,7,5,64,65,44,5];
+// for (let i = 0; i < array.length; i++) {
+//     const element = array[i];
+//     console.log(element);    
+// }
+//  for loop 
+const obj={
+    name:"John",
+    age:30,
+    education:"newyork",
+}
+// for in  object key value iteration
+
+// for (const keys in obj) {
+//      {
+//         const element = obj[keys];
+//         console.log(keys);
+        
+//     }
+// }
+
+
+// for of loop
+// let conn=[78,56,99,55,66]
+// for (const iterator of conn) {
+//     if(iterator==56){
+//         break //terminate the loop
+//     }
+
+// console.log(iterator);
+    
+// }
+
+
+// while loop
+// i=20
+// while(i>5){
+//     console.log(i);
+//     i--;
+// }
+let i=6
+do{
+    console.log(i);
+    i++
+}while(i<5)

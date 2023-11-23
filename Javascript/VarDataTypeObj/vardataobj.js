66fa34c96359a812afa511a0da56dfa05f685890
afide596 <afride596@gmail.com>
2023-11-23 14:22:58 +0530

    Done javascript variables

---
+++ Javascript/VarDataTypeObj/vardataobj.js
@@ -0,0 +1,10 @@
+let employee={
+    "name":"AFRIDE",
+    "job code":5600
+}
+console.log(employee);
+
+employee.name="mohammad";
+console.log(employee);
+employee["job code"]=600;
+console.log(employee);

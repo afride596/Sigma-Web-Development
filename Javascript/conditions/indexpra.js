abcf73e7ee1361f33284cae55c37fafc83a5768a
afide596 <afride596@gmail.com>
2023-11-25 21:29:23 +0530

    done practice on condtion

---
+++ Javascript/conditions/indexpra.js
@@ -0,0 +1,44 @@
+// 1.Use logical operator to find whether the age of the person lies between 10 and 20
+
+// let Age=parseInt(prompt("Enter the Age"));
+
+// if(Age>=10&&Age<=20){
+//     console.log("The entered age is between 10 and 20");
+// }else{
+//     console.log("The entered age is not between 10 and 20");
+// }
+
+// 2.Demonstrate the use of switch case statements in javascript
+// let birthday=3
+// switch (birthday) {
+//     case 1:
+//         console.log("its not my birt")
+        
+//         break;
+//     case 2:
+//         console.log("its my friend birthday");    
+//         break;
+//     case 3:
+//         console.log("its my birthday")    
+
+//     default:
+//         break;
+// }
+// 3.Write a javascript program to find whether a number is divisible by 2 and 3
+
+// let numbers=prompt("enter the number");
+// let number=parseInt(numbers)
+//  if(number%2==0&&number%3==0){
+//     console.log("the number is divisible by both 2 and 3");
+//  }
+//  else{
+//     console.log("the number is not divisble both 2 and 3");
+//  }
+
+//  4.Write a javascript program to find whether a number is divisble by either 2 or 3
+//             
+
+//  5.Print you can drive or you cannot the drive based on age beong grater than 18 using ternary operator 
+
+let age = 17;
+age>=18?console.log('You can drive'):console.log('You cannot drive');

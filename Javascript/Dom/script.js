6574671b6508325ee0100f8e2ea83349c628b427
afide596 <afride596@gmail.com>
2023-12-07 01:13:12 +0530

    just explore dom

---
+++ Javascript/Dom/script.js
@@ -0,0 +1,10 @@
+// By using class selector  
+let cn=document.getElementsByClassName("box")
+cn[0].style.border="2px solid black"
+cn[0].style.backgroundColor="red"
+cn[4].style.backgroundColor="red"
+cn[4].style.width="259px"
+
+// By using Id selector
+let id = document.getElementById('id')
+

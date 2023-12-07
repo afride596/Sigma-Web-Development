6cce9c347a52f9da768bf5c3054bbb2ad33941e3
afide596 <afride596@gmail.com>
2023-12-08 01:46:18 +0530

    completed Exercise

---
+++ Javascript/Exercise/colorANDboxes.js
@@ -0,0 +1,32 @@
+let letters = [
+  "0",
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+];
+// 6 digit representation  of color as hex code
+function randomcolorGenrator() {
+  let colorr = "#";
+  for (let value = 0; value < 6; value++) {
+    let randomcolor = Math.floor(Math.random() * 16);
+    colorr += letters[randomcolor];
+  }
+  return colorr;
+}
+
+document.querySelectorAll(".box").forEach((e) => {
+  e.style.backgroundColor = randomcolorGenrator();
+  e.style.color = randomcolorGenrator();
+});

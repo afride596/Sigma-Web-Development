94e5f36333cf171c2c88f27e71466defb7cf74f9
afide596 <afride596@gmail.com>
2023-12-10 01:03:08 +0530

    modifed

--- Javascript/Dom/script.js
+++ Javascript/Dom/script.js
@@ -1,10 +1,15 @@
 // By using class selector  
-let cn=document.getElementsByClassName("box")
-cn[0].style.border="2px solid black"
-cn[0].style.backgroundColor="red"
-cn[4].style.backgroundColor="red"
-cn[4].style.width="259px"
+// let cn=document.getElementsByClassName("box")
+// cn[0].style.border="2px solid black"
+// cn[0].style.backgroundColor="red"
+// cn[4].style.backgroundColor="red"
+// cn[4].style.width="259px"
 
 // By using Id selector
-let id = document.getElementById('id')
+// let id = document.getElementById('id')
 
+
+
+let con=document.querySelector(".box")
+
+console.log(con.nodeName);

63f3bb610f9dc0e0199e26d2e4b755a766a305b2
afide596 <afride596@gmail.com>
2024-01-07 03:04:45 +0530

    hacking terminal

---
+++ Javascript/Hacking_terminal/script.js
@@ -0,0 +1,29 @@
+
+
+const randomDelay = () => {
+  return new Promise((resolve, reject) => {
+    timeout = Math.random()*6+1;
+    setTimeout(() => {
+      resolve();
+    }, timeout * 1000) ;
+  });
+};
+const additem = async (item) => {
+    await randomDelay();
+    let div = document.createElement("div");
+    div.innerHTML = item;
+    document.body.append(div);
+  };
+let text = [
+  "Initializing Hacking...",
+  "Reading your Files...",
+  "Password files Detected...",
+  "Sending all passwords and personal files to server...",
+  "Cleaning up...",
+];
+
+for (const item of text) {
+  additem(item);
+}
+
+// console.log(timeout);
